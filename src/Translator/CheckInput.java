bb4a1e8e164d2221c7ba701f2af51c3430aaf571
Rickard Jansson <rickard.jansson@gmail.com>
2024-02-07 02:27:12 +0100

    Update: IsLetters checks for . instead of , when string is 1 character

--- src/Translator/CheckInput.java
+++ src/Translator/CheckInput.java
@@ -29,8 +29,9 @@ public class CheckInput {
     }
     public Boolean IsLetters(String input)
     {
-        if(input == null || (input.length()<2 && input.charAt(0) != '?' && input.charAt(0) != ','))
+        if(input == null || (input.length() == 1 && input.charAt(0) == '.'))
             return false;
+
         else
         {
             for (int i = 0; i < input.length(); i++)
@@ -39,6 +40,7 @@ public class CheckInput {
                         && !Character.isSpaceChar(input.charAt(i))
                         && !Character.isDigit(input.charAt(i))
                         && input.charAt(i) != '.'
+                        && (input.length() == 1 && input.charAt(0) == '.')
                         && input.charAt(i) != ','
                         && input.charAt(i) != '?')
                     return false;
